7eb0500683bc1a29bda5b18a7c38fa47b0a75d32
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2025-03-04 21:58:32 +0100

    added sing-in button without menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 // 'use client';
 
-import UserButton from "./ui/components/login/user-button";
+// import Image from "next/image";
+// import googleLogo from "@/public/google.png";
+
+import { GoogleSignInButton } from "./ui/components/login/authButtons";
+// import { getServerSession } from "next-auth";
+
+// import UserButton from "./ui/components/login/user-button";
 // import { SignIn } from "./ui/components/login/auth-component";
 // import { signIn } from "next-auth/react";
 
-export default  function Home() {
+export default async function Home() {
   return (
     <main>
       <h1>Тут буде реєстрація</h1>
-      <UserButton />
+      <GoogleSignInButton />
+      {/* <UserButton /> */}
       {/* <button onClick={() => signIn("google")}>Sign in with Google</button> */}
       {/* <button
         onClick={() => signIn("google", { callbackUrl: "/flow/dashboard" })}
